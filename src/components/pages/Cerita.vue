3b9141d25c76c4626feb6a6c8f160e11ca6893b8
wiku <wiku@gruneasia.info>
2020-12-25 14:59:41 +0700

    add sort by popularity and newest function

--- src/components/pages/Cerita.vue
+++ src/components/pages/Cerita.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
-    <h3 class="judul">Cerita Terbaru</h3>
+    <div class="sorting__container">
+      <h4>Sorting berdasarkan</h4>
+      <div class="sorting__button__container">
+        <button :class="newestActive ? 'active' : ''" @click="newestSortHandle">Paling baru</button>
+        <button :class="MostPopularActive ? 'active' : ''" @click="popularitySortHandle">Paling Popluer</button>
+      </div>
+    </div>
+
+    <h3 class="judul">{{judulCerita}}</h3>
 
     <CeritaTemplate
       v-for="cerita in ceritas"
@@ -22,15 +30,39 @@ export default {
   data() {
     return {
       ceritas: [],
+      judulCerita : 'Cerita Terbaru',
+      newestActive : true,
+      MostPopularActive : false,
     }
   },
   mounted() {
     axios.get("http://127.0.0.1:8000/api/cerita")
     .then(res => {
       this.ceritas = res.data.ceritas;
-      console.log(this.ceritas);
     })
     .catch(err => console.log(err))
+  },
+  methods: {
+    popularitySortHandle(){
+      axios.get("http://127.0.0.1:8000/api/cerita/popular-sort")
+    .then(res => {
+      this.ceritas = res.data.ceritas;
+      this.judulCerita = 'Cerita Terpopuler';
+      this.newestActive = false;
+      this.MostPopularActive = true;
+    })
+    .catch(err => console.log(err))
+    },
+    newestSortHandle(){
+      axios.get("http://127.0.0.1:8000/api/cerita/")
+    .then(res => {
+      this.ceritas = res.data.ceritas;
+      this.judulCerita = 'Cerita Terbaru';
+      this.newestActive = true;
+      this.MostPopularActive = false;
+    })
+    .catch(err => console.log(err))
+    },
   }
 }
 </script>
@@ -38,5 +70,32 @@ export default {
 <style scoped>
 .judul {
   width: 50vw;
+}
+
+.sorting__container{
+  display: flex;
+  align-items: center;
+}
+
+.sorting__button__container{
+  margin-left: 10px;
+}
+
+.sorting__button__container button{
+  margin-left: 5px;
+  margin-right: 5px;
+  padding: 10px;
+  background-color: darkgrey;
+  border-color: darkgrey !important;
+  border-radius: 5px;
+  cursor: pointer;
+  border-style: none;
+  font-weight: bold;
+}
+
+.sorting__button__container button.active{
+  background-color: black;
+  border-color: black;
+  color: #fff;
 }
 </style>
